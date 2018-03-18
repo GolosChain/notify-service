5e06855b1bf7522f6924c013338101446f0c7563
Alex Gu <sualexx@aol.com>
2018-03-18 17:06:00 +0300

    Stable queue put

--- packages/golos.proxy/index.js
+++ packages/golos.proxy/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable quotes */
 import {Golos} from 'golos.lib';
 import {Queue} from 'golos.lib';
+import punycode from 'punycode';
 
 // Data flow:
 // <-- stream of events from the sniffer
@@ -32,19 +33,31 @@ import {Queue} from 'golos.lib';
 //   https://www.codesd.com/item/how-to-take-an-array-of-strings-and-get-the-ascii-value-of-each-character-in-each-string-in-java.html
 
 
-// const q = new Queue({port: 3301});
-// q.on('connect', async() => {
-//   const ok = await q.assertTube('q2', 'fifo');
-//   const stat = await q.statistics();
-//   const put = await q.put('q2', 'blaaaaaaaaaaaa');
-//   console.log(`put:`, put);
-//   const taken = await q.take('q2', 10);
-//   const takenId = taken[0];
-//   console.log(`taken:`, taken);
-//   console.log(`taken id : ${takenId}`)
-//   const acked = await q.ack('q2', takenId);
-//   console.log(`acked:`, acked);
-// });
+const qName = userId => punycode.ucs2
+  .decode(userId)
+  .map(c => `_${c.toString(16)}`)
+  .join('');
+
+
+const q = new Queue({port: 3301});
+
+q.on('connect', async() => {
+  // const ok = await q.assertTube('q2', 'fifo');
+  // const stat = await q.statistics();
+  // console.log(stat);
+  // const put = await q.put({
+  //   tube_name: 'q2',
+  //   task_data: 'blaaaaaaaaaaaa'
+  // });
+  // console.log(`put:`, put);
+  // const taken = await q.take('q2', 10);
+  // const takenId = taken[0];
+  // console.log(`taken:`, taken);
+  // console.log(`taken id : ${takenId}`);
+  // const acked = await q.ack('q2', takenId);
+  // console.log(`acked:`, acked);
+  console.log(`[Queue] connected`);
+});
 
 
 const start = async() => {
@@ -53,72 +66,81 @@ const start = async() => {
   blocks
     .subscribe(
       async block => {
-        console.log(`------------------------------------------------------- [ ${block.index} ]`);
-        console.log(`[ops count : ] ${block.operations.length}`);
-        block.operations.map(op => {
-          console.log(`<<<<[${op.type}]`);
-          console.log(`[author][${op.author}]`);
-          console.log(`[parent_author][${op.parent_author}]`);
-          console.log(`[to][${op.to}]`);
-          console.log(`<<<<[target] -> [${op.target}]`);
-        });
+        console.log(`[processing] ${block.index}`);
+        for (const op of block.operations) {
+          const {target} = op;
+          const qn = qName(target);
+          console.log(qn);
+          await q.assertTube(qn, 'fifo');
+          const put = await q.put({
+            tube_name: qn,
+            task_data: op.type
+          });
+          console.log(put);
+        }
+        console.log(`ok`);
+
+
       }
     );
-
-  // block.subscribe(
-  //   async x => {
-  //     console.log(`-------------------------------------------------------------------[ ${x.index} ]`);
-  //   // console.log(`[ put block number into queue : ${tChainName} ]`);
-  //   // const put = await queue.put(tChainName, x.index);
-  //   // console.log(`put:`, put);
-  //   // const taken = await queue.take(tChainName);
-  //   // const takenId = taken[0];
-  //   // console.log(`taken:`, taken);
-  //   // console.log(`taken id : ${takenId}`)
-  //   // const acked = await queue.ack(tChainName, takenId);
-  //   // console.log(`acked:`, acked);
-  //   });
-
-
-  // const queue = new Queue({port: 3301});
-  // // top level queue (utube) - namespace for the blockchain subqueues
-  // const tChainName = 'main';
-  // const tHeadName = 'head';
-  // console.log(`<<< asserting tube: ${tChainName}`);
-  // const tChain = await queue.assertTube(tChainName, 'fifo');
-  // console.log(`<<< tube: ${tChainName} is ${tChain}`);
-  // //
-
-
-  // function toluaOptions(optionsObject) {
-  //   const result = '{}';
-  //   try {
-  //
-  //     for (const op in optionsObject) {
-  //       console.log(typeof optionsObject[op]);
-  //     }
-  //
-  //     // const oString = JSON.stringify(optionsObject);
-  //     // console.log(oString);
-  //     // result = oString.replace(/:/g, '= ').replace(/"/g, ' ');
-  //     // console.log(result);
-  //     // return result;
-  //   } catch (e) {
-  //     //  log something about wrong object?
-  //   } finally {
-  //     return result;
-  //   }
-  // }
-  //
-  // toluaOptions({ttl: 60.1, delay: 80, utube: 'bla'});
-
-
-  // console.log(toluaOptions({ttl: 60.1, delay: 80}));
-
-
 };
 
 start();
+
+// block.subscribe(
+//   async x => {
+//     console.log(`-------------------------------------------------------------------[ ${x.index} ]`);
+//   // console.log(`[ put block number into queue : ${tChainName} ]`);
+//   // const put = await queue.put(tChainName, x.index);
+//   // console.log(`put:`, put);
+//   // const taken = await queue.take(tChainName);
+//   // const takenId = taken[0];
+//   // console.log(`taken:`, taken);
+//   // console.log(`taken id : ${takenId}`)
+//   // const acked = await queue.ack(tChainName, takenId);
+//   // console.log(`acked:`, acked);
+//   });
+
+
+// const queue = new Queue({port: 3301});
+// // top level queue (utube) - namespace for the blockchain subqueues
+// const tChainName = 'main';
+// const tHeadName = 'head';
+// console.log(`<<< asserting tube: ${tChainName}`);
+// const tChain = await queue.assertTube(tChainName, 'fifo');
+// console.log(`<<< tube: ${tChainName} is ${tChain}`);
+// //
+
+
+// function toluaOptions(optionsObject) {
+//   const result = '{}';
+//   try {
+//
+//     for (const op in optionsObject) {
+//       console.log(typeof optionsObject[op]);
+//     }
+//
+//     // const oString = JSON.stringify(optionsObject);
+//     // console.log(oString);
+//     // result = oString.replace(/:/g, '= ').replace(/"/g, ' ');
+//     // console.log(result);
+//     // return result;
+//   } catch (e) {
+//     //  log something about wrong object?
+//   } finally {
+//     return result;
+//   }
+// }
+//
+// toluaOptions({ttl: 60.1, delay: 80, utube: 'bla'});
+
+
+// console.log(toluaOptions({ttl: 60.1, delay: 80}));
+
+
+// };
+
+// start();
 
 // const blockOps =
 //   blockTrxs
