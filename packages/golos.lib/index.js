f253f7450e1c53515ab13eab1bc5fdd38f706971
Alex Gu <sualexx@aol.com>
2018-03-30 18:58:05 +0300

    Refactor

--- packages/golos.lib/index.js
+++ packages/golos.lib/index.js
@@ -1,8 +1,2 @@
-// export {default as PersistentWebSocket} from './data/source/transport/ws/persistent';
-
-export { default as WebSocket } from './data/transport/WebSocket';
-export { default as PersistentWebSocket } from './data/transport/WebSocket/Persistent';
-export {default as ObservableWebSocket} from './data/transport/WebSocket/observable';
-export {default as Golos} from './data/chain/golos';
-// export {default as Tarantool} from './db/tarantool';
+export {default as Golos} from './chains/golos/proxy';
 export {default as Queue} from './queue/tarantool/Queue';
