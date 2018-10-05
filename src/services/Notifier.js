11363df1932d7868d9e3bf520e571a1f80707bd2
Oleg <oleg.pav.m@gmail.com>
2018-10-05 12:36:54 +0300

    #19 Change event-notification format #2.

--- src/services/Notifier.js
+++ src/services/Notifier.js
@@ -9,81 +9,53 @@ class Notifier extends BasicService {
 
         this._connector = connector;
         this._registrator = registrator;
-        this._accumulator = new Map(); // user -> type -> [data]
+        this._accumulator = {};
     }
 
     async start() {
-        this._generateListeners();
+        this._registrator.on('registerEvent', this._accumulateEvent.bind(this));
+        this._registrator.on('blockDone', this._broadcast.bind(this));
     }
 
     async stop() {
         await this.stopNested();
     }
 
-    _generateListeners() {
-        let fn;
+    async _accumulateEvent(user, data) {
+        data = Object.assign({}, data);
 
-        for (let eventType of eventTypes) {
-            switch (eventType) {
-                case 'transfer':
-                    fn = (user, data) => this._accumulate(user, eventType, data);
-                    break;
-                default:
-                    fn = (user, data) => this._accumulateWithIncrement(user, eventType, data);
-                    break;
-            }
+        delete data.__v;
+        delete data.blockNum;
+        delete data.user;
 
-            this._registrator.on(eventType, fn);
-        }
-
-        this._registrator.on('blockDone', this._broadcast.bind(this));
-    }
-
-    _accumulate(user, type, data) {
-        this._accumulatorBy(user, type).add(data);
-    }
-
-    _accumulateWithIncrement(user, type, data) {
-        const acc = this._accumulatorBy(user, type);
-
-        if (acc.size) {
-            acc.values().next().value.counter++;
-        } else {
-            acc.add({ counter: 1, ...data });
-        }
-    }
-
-    _accumulatorBy(user, type) {
-        const acc = this._accumulator;
-
-        if (!acc.get(user)) {
-            acc.set(user, new Map());
-        }
-
-        if (!acc.get(user).get(type)) {
-            acc.get(user).set(type, new Set());
-        }
-
-        return acc.get(user).get(type);
-    }
-
-    _cleanAccumulator() {
-        this._accumulator = new Map();
+        this._accumulator[user] = this._accumulator[user] || [];
+        this._accumulator[user].push(data);
     }
 
     async _broadcast() {
         const time = new Date();
-        const result = this._prepareBroadcastData();
+        const accumulator = this._accumulator;
 
+        this._accumulator = {};
+
+        await this._sendToOnlineNotify(accumulator);
+        await this._sendToPush(accumulator);
+
+        stats.timing('broadcast_notify', new Date() - time);
+    }
+
+    async _sendToOnlineNotify(accumulator) {
         try {
-            await this._connector.sendTo('onlineNotify', 'transfer', result);
+            await this._connector.sendTo('onlineNotify', 'transfer', accumulator);
         } catch (error) {
             stats.increment('broadcast_to_online_notifier_error');
             Logger.error(`On send to online notifier - ${error}`);
         }
+    }
 
+    async _sendToPush(accumulator) {
         try {
-            await this._connector.sendTo('push', 'transfer', result);
+            await this._connector.sendTo('push', 'transfer', accumulator);
         } catch (error) {
             stats.increment('broadcast_to_push_error');
             Logger.error(`On send to push - ${error}`);
